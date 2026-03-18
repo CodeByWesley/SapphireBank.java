e11ae711e99bb42f14dd1e50010e2a597f3da6cc
CodeByWesley <wesleyopip@gmail.com>
2026-03-18 20:56:07 -0300

    renomenado terminalAccout para Main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class TerminalAccount {
+public class Main {
     public static void main (String [] args){
         System.out.println("=================================");
         System.out.println("\"Welcome to Sapphire Bank!\"");
